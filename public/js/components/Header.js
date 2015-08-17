d6763a677d476d1204b995deaad2888878ddb170
Matt Harrison <hi@matt-harrison.com>
2015-08-17 07:00:42 +0100

    Dynamic header

--- public/js/components/Header.js
+++ public/js/components/Header.js
@@ -1,9 +1,37 @@
 var Link = require('react-router').Link;
 var React = require('react');
+var Wreck = require('wreck');
 
 var Header = module.exports = React.createClass({
 
+    getInitialState: function () {
+
+        return {};
+    },
+
+    componentDidMount: function () {
+
+        var self = this;
+
+        Wreck.get('/account', { json: 'force' }, function (err, res, payload) {
+
+            if (err) {
+                throw err;
+            }
+
+            self.setState({ account: payload });
+        });
+    },
+
     render: function () {
+
+        var login = this.state.account ? 
+            <li><a href="/logout">Signed in as {this.state.account.displayName}. Log out?</a></li> :
+            <li><a href="/login">Sign in with Github</a></li>;
+
+        var request = this.state.account ?
+            <li><Link to="page" params={{ name: 'request '}}>Request video</Link></li> :
+            '';
 
         return  (
             <header>
@@ -16,9 +44,9 @@ var Header = module.exports = React.createClass({
                     <nav className="columns large-12">
                         <ul>
                             <li><Link to="page" params={{ name: 'about '}}>What is this?</Link></li>
-                            <li><Link to="page" params={{ name: 'request '}}>Request video</Link></li>
+                            {request}
                             |
-                            <li><a href="/login">Sign in with Github</a></li>
+                            {login}
                         </ul>
                     </nav>
                 </div>
